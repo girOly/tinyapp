f55cd099554b7941b32a80387df8eb3c44a96831
girOly <girardob@gmail.com>
2019-07-10 21:46:11 +0000

    Implemented Login/Register

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const app = express(); // Express Framework
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const {
+  findEmail,
+  generateRandomString,
+  addNewUser,
+  checkPassword
+} = require("./functions");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +18,19 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
 
 app.get("/", (req, res) => {
@@ -30,22 +49,44 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  userId = req.cookies["user_id"] ? req.cookies["user_id"] : "";
+  let templateVars = { urls: urlDatabase, user: users[userId] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  username = req.cookies["username"] ? req.cookies["username"] : "";
-  let templateVars = { urls: urlDatabase, username };
+  userId = req.cookies["user_id"] ? req.cookies["user_id"] : "";
+  let templateVars = { urls: urlDatabase, user: users[userId] };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  username = req.cookies["username"] ? req.cookies["username"] : "";
+  userId = req.cookies["user_id"] ? req.cookies["user_id"] : "";
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username
+    user: users[userId]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  userId = req.cookies["user_id"] ? req.cookies["user_id"] : "";
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    user: users[userId]
+  };
+  res.render("urls_register", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  userId = req.cookies["user_id"] ? req.cookies["user_id"] : "";
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    user: users[userId]
+  };
+  res.render("urls_login", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -57,12 +98,6 @@ app.post("/urls", (req, res) => {
   // res.send("Redirection in progres.."); // Respond with 'Ok' (we will replace this)
   res.redirect(`/urls/${shortURL}`);
 });
-
-function generateRandomString() {
-  return Math.random()
-    .toString(36)
-    .substr(2, 5);
-}
 
 app.get("/u/:shortURL", (req, res) => {
   // console.log(req.params);
@@ -81,11 +116,47 @@ app.post("/urls/:id/edit", (req, res) => {
   urlDatabase[id] = req.body.longURL;
   res.redirect("/urls");
 });
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+
+// app.post("/login", (req, res) => {
+//   res.cookie("username", req.body.username);
+//   res.redirect("/urls");
+// });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+
+app.post("/register", (req, res) => {
+  let { email, password } = req.body;
+  if (email === "" || password === "") {
+    res.end("400 Error");
+  } else if (findEmail(email, users)) {
+    // Add email/password to the ID Object
+    res.end("400 Error Email");
+  } else if (!findEmail(email, users)) {
+    let id = addNewUser(email, password, users);
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  }
+});
+
+app.post("/login", (req, res) => {
+  let { email, password } = req.body;
+  if (email === "" || password === "") {
+    return "400 Error";
+  } else if (findEmail(email, users)) {
+    // Check Email - Password
+    console.log("Were almost there");
+    if (checkPassword(password, users)) {
+      console.log("We never make it to here");
+        let id = addNewUser(email, password, users);
+      res.cookie("user_id", id);
+      res.redirect("/urls");
+    } else {
+      res.end("400 Error ");
+    }
+  } else if (!findEmail(email, users)) {
+    res.write("403 Error");
+  }
 });
