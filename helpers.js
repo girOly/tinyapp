43d20ea5442cdcb1ed2d557b60d24bd2b68e517c
girOly <girardob@gmail.com>
2019-07-12 01:05:13 +0000

    Project Finalisation

--- helpers.js
+++ helpers.js
@@ -1,9 +1,11 @@
 const bcrypt = require("bcrypt");
+// Generates Random URL ID
 const generateRandomString = function() {
   return Math.random()
     .toString(36)
     .substr(2, 5);
 };
+// Checks Password inout with Hashed Database
 const checkPassword = function(password, users) {
   for (let userId in users) {
     if (bcrypt.compareSync(password, users[userId].password)) {
@@ -12,7 +14,7 @@ const checkPassword = function(password, users) {
   }
   return false;
 };
-
+// Finds Email in Users Database
 const findEmail = function(email, users) {
   for (let user in users) {
     if (email === users[user].email) {
@@ -22,7 +24,7 @@ const findEmail = function(email, users) {
   }
   return false;
 };
-
+// Adds new User to Database
 const addNewUser = function(email, password, users) {
   let salt = bcrypt.genSaltSync(10);
   let id = generateRandomString();
@@ -30,7 +32,7 @@ const addNewUser = function(email, password, users) {
   users[id] = { id, email, password: hashedPassword };
   return id;
 };
-
+// Filters URL Database with logged User
 const filterUrls = function(userId, urlDatabase) {
   const filteredData = {};
   for (let shortURLs in urlDatabase) {
@@ -41,6 +43,7 @@ const filterUrls = function(userId, urlDatabase) {
   return filteredData;
 };
 
+// Gets User ID/Session by Email
 const getUserByEmail = function(email, users) {
   for (let userId in users) {
     if (email === users[userId].email) {
@@ -50,6 +53,7 @@ const getUserByEmail = function(email, users) {
   return undefined
 };
 
+// ---- Module Exports
 module.exports = {
   generateRandomString,
   findEmail,
